299150c6f3b9de515e2e3b2e29633aac52611417
yehuda <yudagn@gmail.com>
2021-09-29 01:44:45 -0400

    add random linear gradient to random tree generator

--- src/random_branch.js
+++ src/random_branch.js
@@ -13,7 +13,27 @@ class RandomBranch {
   drawBranch() {
     this.ctx.beginPath();
     this.ctx.save();
-    this.ctx.strokeStyle = this.color;
+    // this.ctx.strokeStyle = this.color;
+
+    let color1 = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+      Math.random() * 255
+    })`;
+    let color2 = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+      Math.random() * 255
+    })`;
+    let color3 = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+      Math.random() * 255
+    })`;
+
+    let grd = this.ctx.createLinearGradient(0, -this.len, this.width, this.len);
+    grd.addColorStop(0, color1);
+    grd.addColorStop(.5, color2);
+    grd.addColorStop(1, color3);
+    
+    // let grd = this.ctx.createRadialGradient(0,-this.len,this.width/4,this.width/2,this.len/2,this.width);
+    // grd.addColorStop(0,"red");
+    // grd.addColorStop(1,"blue");
+    this.ctx.strokeStyle = grd;
 
     // glow
     this.ctx.shadowBlur = Math.floor(Math.random() * 20) + 20;
